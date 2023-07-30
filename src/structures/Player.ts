eb7a0eec71c88e5f707c635a98dbe2d377ae5c9a
Davi Patricio <davipatricio@pm.me>
2023-07-30 18:16:25 -0300

    add docs to Player class

--- src/structures/Player.ts
+++ src/structures/Player.ts
@@ -15,14 +15,26 @@ export default class Player {
   state: PlayerState = PlayerState.Handshaking;
   constructor(public socket: Socket, public name: string | null = null) {}
 
+  /**
+   * Sets the player's state. Should not be changed manually.
+   * @param state The state to set the player to.
+   */
   setState(state: PlayerState) {
     this.state = state;
   }
 
+  /**
+   * Sends a Packet to the player.
+   * @param packet The packet to send to the player.
+   */
   sendPacket(packet: Packet) {
     this.socket.write(packet.getBuffer());
   }
 
+  /**
+   * Kicks the player from the server with the specified reason.
+   * @param reason The reason the player is being kicked. e.g. `§cYou have been kicked.`
+   */
   kick(reason: string) {
     const packet = new Packet()
       .setID(writeVarInt(this.state === PlayerState.Login ? 0x00 : 0x1a))
@@ -32,6 +44,9 @@ export default class Player {
     this.disconnect();
   }
 
+  /**
+   * Drops the connection with the player immediately.
+   */
   disconnect() {
     this.socket.destroy();
   }
